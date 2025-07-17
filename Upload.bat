ad0d1b0da438943045eb991c90a75deb120dacc8
Sublustrum007 <Sublustrum007@yandex.ru>
2025-07-17 14:55:07 +0300

    Create file Upload.bat SKANER 17/07/2025 14:55:07

---
+++ Upload.bat
@@ -0,0 +1,6 @@
+git add ./ 
+timeout /t 15 /nobreak 
+set /p commit="Please enter the commit: " 
+git commit -m "%commit% %Computername% %date:~0,2%/%date:~3,2%/%date:~-4% %time:~0,2%:%time:~3,2%:%time:~6,2%" 
+timeout /t 15 /nobreak 
+git push https://github.com/Sublustrum007ru/prog1.git 
