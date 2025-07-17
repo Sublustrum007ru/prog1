6fe3b81c83e5f6ee983bfc0d3ea35bcdc6ce6cc7
Sublustrum007 <Sublustrum007@yandex.ru>
2025-07-17 14:55:19 +0300

    Create file Application.java SKANER 17/07/2025 14:55:19

---
+++ src/view/Application.java
@@ -0,0 +1,10 @@
+package view; 
+public class Application{ 
+    public static void run(){ 
+        String helloMSG = "Hello World!!!\nMy name is Sublustrum007"; 
+        promt(helloMSG);  
+    } 
+    public static void promt(String str){ 
+        System.out.println(str); 
+    } 
+} 
