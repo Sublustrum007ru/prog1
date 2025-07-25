e0fe7047aee0dac6adabc05b53f7008dc4427194
Sublustrum007 <Sublustrum007@yandex.ru>
2025-07-25 19:57:47 +0300

    Update SKANER 25/07/2025 19:57:46

--- Upload.bat
+++ Upload.bat
@@ -3,4 +3,5 @@ timeout /t 15 /nobreak
 set /p commit="Please enter the commit: " 
 git commit -m "%commit% %Computername% %date:~0,2%/%date:~3,2%/%date:~-4% %time:~0,2%:%time:~3,2%:%time:~6,2%" 
 timeout /t 15 /nobreak 
-git push https://github.com/Sublustrum007ru/prog1.git 
+git push https://github.com/Sublustrum007ru/prog1.git
+git push https://github.com/Sublustrum007ru/prog1
